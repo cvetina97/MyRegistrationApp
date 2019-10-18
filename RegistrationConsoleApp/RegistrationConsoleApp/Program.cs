f7a34e6bc5541353f4d8df017f94db1627c3e43f
cvetina97 <cvetina97@outlook.com>
2019-10-18 14:14:25 +0300

    Initial login , register flow !

--- RegistrationConsoleApp/RegistrationConsoleApp/Program.cs
+++ RegistrationConsoleApp/RegistrationConsoleApp/Program.cs
@@ -7,6 +7,33 @@ namespace RegistrationConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Press 1 for Registration or 2 for Login!");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    RegisterUser();
+                    break;
+                case "2":
+                    LoginUser();
+                    break;
+                default:
+                    Console.WriteLine("Go fuck yourself!");
+                    break;
+            }
+
+            //Console.WriteLine("Username");
+            //string username = Console.ReadLine();
+            //Console.WriteLine("Password");
+            //string password = Console.ReadLine();
+
+            //insert data in DB
+            
+        }
+
+        private static void LoginUser()
+        {
             List<string> names = new List<string>()
             {
                 "Zaiko Baiko ",
@@ -15,12 +42,15 @@ namespace RegistrationConsoleApp
             };
 
 
-            foreach(var name in names)
+            foreach (var name in names)
             {
                 Console.WriteLine(name);
             }
+        }
 
-            
+        private static void RegisterUser()
+        {
+            throw new NotImplementedException();
         }
     }
 }
